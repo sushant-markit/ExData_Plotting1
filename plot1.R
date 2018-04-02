76a831844dc3dd7bc976c1343ff082c466715031
Sushant Aggarwal <sushant.aggarwal@markit.com>
2018-04-02 06:13:09 +0100

    project 1 files

---
+++ plot1.R
@@ -0,0 +1,9 @@
+File <- "./data/household_power_consumption.txt"
+dataSet <- read.table(File, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- dataSet[dataSet$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
